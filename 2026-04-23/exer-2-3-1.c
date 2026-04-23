2649fc708a3b3a4005fb572ab43cc5ea7dd518d1
kanekanekanea <kmc2618@kamiyama.ac.jp>
2026-04-23 15:00:10 +0900

    コミットするよ

--- 2026-04-23/exer-2-3-1.c
+++ 2026-04-23/exer-2-3-1.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
-int main(void){
-    printf("Good afternoon");
-    return 0;
+int main(void) {
+
+
+    int age = 15;
+    char name[16] = "〜〜";
+    
+	char favorite1[16] = "ゲーム制作";
+	char favorite2[7] = "木刀";
+	char favorite3[13] = "マンゴー";
+	
+	printf("##Hello World\n");
+    printf("%d歳の%sです。\n",age,name);
+	printf("%s\n", favorite1);
+	printf("%s\n", favorite2);
+	printf("%s\n", favorite3);
+    printf("が好きです");
+
+	return 0;
 }
+
